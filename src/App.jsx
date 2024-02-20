882bcaabcd5358fd7981b0d5e8696070ec3cb081
Monteloyola, Carole <carole.monteloyola@sap.com>
2024-02-20 13:27:14 +0800

    added stacks and instances panels and dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,13 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("");
-  const [hasResults, setHasResults] = useState(false);
-
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
+  const [showInstances, setShowInstances] = useState(false);
 
   const handleClick = () => {
-    if (text.trim() !== "") {
-      setHasResults(true);
+    if (!showInstances) {
+      setShowInstances(true);
     } else {
-      setHasResults(false);
+      setShowInstances(false);
     }
   };
 
@@ -26,119 +21,82 @@ function App() {
 
       <div className="container">
         <div className="panel">
-          <h2>Instance</h2>
+          <h2>Stacks</h2>
 
-          <div className="config">
-            <span>Stacks:</span>
-            <input type="text" size={14} value={text} onChange={handleChange} />
-            <button onClick={handleClick}>Search</button>
-          </div>
-
-          {hasResults && (
-            <div className="config_container">
-              <div className="config">
-                <span>Host Name</span>
-                <p>hostname here</p>
-              </div>
-              <div className="config">
-                <span>Instance Type</span>
-                <p>instance type here</p>
-              </div>
-              <div className="config">
-                <span>IP Address</span>
-                <p>ip address here</p>
-              </div>
-              <div className="config">
-                <span>Availability Zone</span>
-                <p>az here</p>
-              </div>
-            </div>
-          )}
+          <table>
+            <thead>
+              <tr>
+                <th>StackName</th>
+                <th>ConfigType</th>
+                <th>Environment</th>
+                <th>SQLVersion</th>
+                <th>Timestamp</th>
+                <th>CreatorName</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>StackName</td>
+                <td>ConfigType</td>
+                <td>Environment</td>
+                <td>SQLVersion</td>
+                <td>Timestamp</td>
+                <td>CreatorName</td>
+                <td>
+                  <button onClick={handleClick}>Instances</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        <div className="panel panel_details">
-          <h2>History</h2>
-
-          {hasResults && (
+        {showInstances && (
+          <div className="panel">
+            <div className="panel-header">
+              <h2>Instances</h2>
+              <button className="close" onClick={handleClick}>
+                X
+              </button>
+            </div>
             <table>
               <thead>
                 <tr>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Event</th>
-                  <th>Username</th>
-                  <th>Full Log</th>
+                  <th>InstanceId</th>
+                  <th>HostName</th>
+                  <th>InstanceType</th>
+                  <th>PrivateIP</th>
+                  <th>AZ</th>
+                  <th>RoleType</th>
+                  <th>CreatorName</th>
+                  <th>CreateDate</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>February 20, 2024</td>
-                  <td>5:49 AM</td>
-                  <td>CreateTags</td>
-                  <td>cmonteloyola</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
+                  <td>InstanceId</td>
+                  <td>HostName</td>
+                  <td>InstanceType</td>
+                  <td>PrivateIP</td>
+                  <td>AZ</td>
+                  <td>RoleType</td>
+                  <td>CreatorName</td>
+                  <td>CreateDate</td>
                 </tr>
                 <tr>
-                  <td>January 15, 2024</td>
-                  <td>1:00 PM</td>
-                  <td>Configuration Change</td>
-                  <td>rrestauro</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>February 20, 2024</td>
-                  <td>5:49 AM</td>
-                  <td>CreateTags</td>
-                  <td>cmonteloyola</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>January 15, 2024</td>
-                  <td>1:00 PM</td>
-                  <td>Configuration Change</td>
-                  <td>rrestauro</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>February 20, 2024</td>
-                  <td>5:49 AM</td>
-                  <td>CreateTags</td>
-                  <td>cmonteloyola</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>January 15, 2024</td>
-                  <td>1:00 PM</td>
-                  <td>Configuration Change</td>
-                  <td>rrestauro</td>
-                  <td>
-                    <a href="">Cloudtrail</a>
-                  </td>
+                  <td>InstanceId</td>
+                  <td>HostName</td>
+                  <td>InstanceType</td>
+                  <td>PrivateIP</td>
+                  <td>AZ</td>
+                  <td>RoleType</td>
+                  <td>CreatorName</td>
+                  <td>CreateDate</td>
                 </tr>
               </tbody>
             </table>
-          )}
-        </div>
-
-        {/* <div className="panel panel_logs">
-          <h2>Logs</h2>
-
-          <div className="logs">
-            {
-              '{Configuration.NetworkInterfaces.0: {"attachment":{"attachTime":"2024-02-19T21:26:57.000Z","attachmentId":"eni-attach-01dbb98ba4bca9f20","deleteOnTermination":true,"deviceIndex":0,"status":"attached","networkCardIndex":0},"description":"","groups":[{"groupName":"argo-cluster-nodegroup-20240219-212544-NodeSecurityGroup-NSK66Q7MOJFB","groupId":"sg-033796fde3449658c"},{"groupName":"default","groupId":"sg-06adad16037b67f1d"}],"ipv6Addresses":[],"macAddress":"02:8a:5c:70:a3:31","networkInterfaceId":"eni-0e7f23dd3306fb946","ownerId":"957236237862","privateDnsName":"ip-10-14-174-167.us-west-2.compute.internal","privateIpAddress":"10.14.174.167","privateIpAddresses":[{"primary":true,"privateDnsName":"ip-10-14-174-167.us-west-2.compute.internal","privateIpAddress":"10.14.174.167"}],"sourceDestCheck":true,"status":"in-use","subnetId":"subnet-07ff3e302dc23b377","vpcId":"vpc-0b2710d480e87ce5c","interfaceType":"interface"}}'
-            }
           </div>
-        </div> */}
+        )}
       </div>
     </>
   );
